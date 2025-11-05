3ac53255b4e1901bc5af2935bf29b78ce6e5db50
mehtayuvraj <yuvraj.mehta@aagati.com>
2025-11-05 13:06:20 +0530

    Fix port for Render

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 from fastapi import FastAPI
 from router import router
+import os
 
 app = FastAPI(
     title="ATS Resume Scoring API",
@@ -15,4 +16,5 @@ app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
